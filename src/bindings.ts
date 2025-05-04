ac7c03e05d5e6cbfa2ebe8f1c28a432ebdf7a125
maamokun <sachadeco214@gmail.com>
2025-05-04 16:57:15 +0900

    fix: format code and improve caching logic for user and server avatars

--- src/bindings.ts
+++ src/bindings.ts
@@ -1,5 +1,5 @@
 export type Bindings = {
-	UPSTASH_REDIS_URL: string;
-	UPSTASH_REDIS_TOKEN: string;
-	DISCORD_TOKEN: string;
+    UPSTASH_REDIS_URL: string;
+    UPSTASH_REDIS_TOKEN: string;
+    DISCORD_TOKEN: string;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import { cors } from 'hono/cors';
+import { cors } from "hono/cors";
 import { Redis } from "@upstash/redis/cloudflare";
 import { type Bindings } from "./bindings";
 
@@ -8,47 +8,88 @@ const app = new Hono<{ Bindings: Bindings }>();
 app.use(cors());
 
 app.get("/", (c) => {
-	return c.redirect("https://github.com/mikndotdev/discord-avatar", 302);
+    return c.redirect("https://github.com/mikndotdev/discord-avatar", 302);
 });
 
 app.get("/:id", async (c) => {
-	const id = c.req.param("id");
-	const resolution = c.req.query("resolution") || "1024";
-	const format = c.req.query("format") || "png";
+    const id = c.req.param("id");
+    const resolution = c.req.query("resolution") || "1024";
+    const format = c.req.query("format") || "png";
 
-	const redis = new Redis({
-		url: c.env.UPSTASH_REDIS_URL,
-		token: c.env.UPSTASH_REDIS_TOKEN,
-	});
+    const redis = new Redis({
+        url: c.env.UPSTASH_REDIS_URL,
+        token: c.env.UPSTASH_REDIS_TOKEN,
+    });
 
-	const get = async (id: string) => {
-		const cacheRes = await redis.get(id);
-		if (cacheRes) {
-			return cacheRes;
-		} else {
-			const res = await fetch(`https://discord.com/api/v10/users/${id}`, {
-				headers: {
-					Authorization: `Bot ${c.env.DISCORD_TOKEN}`,
-				},
-			});
-			if (!res.ok) {
-				return null;
-			}
-			console.log(res);
-			const data = (await res.json()) as { avatar: string };
-			const avatar = data.avatar;
-			await redis.set(id, avatar, { ex: 60 });
-			return avatar;
-		}
-	};
-	const avatar = await get(id);
-	if (!avatar) {
-		return c.json({ error: "User not found" }, { status: 404 });
-	}
-	return c.redirect(
-		`https://cdn.discordapp.com/avatars/${id}/${avatar}.${format}?size=${resolution}`,
-		302,
-	);
+    const get = async (id: string) => {
+        const cacheRes = await redis.get(`user-${id}`);
+        if (cacheRes) {
+            return cacheRes;
+        } else {
+            const res = await fetch(`https://discord.com/api/v10/users/${id}`, {
+                headers: {
+                    Authorization: `Bot ${c.env.DISCORD_TOKEN}`,
+                },
+            });
+            if (!res.ok) {
+                return null;
+            }
+            const data = (await res.json()) as { avatar: string };
+            const avatar = data.avatar;
+            await redis.set(`user-${id}`, avatar, { ex: 60 });
+            return avatar;
+        }
+    };
+    const avatar = await get(id);
+    if (!avatar) {
+        return c.json({ error: "User not found" }, { status: 404 });
+    }
+    return c.redirect(
+        `https://cdn.discordapp.com/avatars/${id}/${avatar}.${format}?size=${resolution}`,
+        302,
+    );
+});
+
+app.get("/server/:id", async (c) => {
+    const id = c.req.param("id");
+    const resolution = c.req.query("resolution") || "1024";
+    const format = c.req.query("format") || "png";
+
+    const redis = new Redis({
+        url: c.env.UPSTASH_REDIS_URL,
+        token: c.env.UPSTASH_REDIS_TOKEN,
+    });
+
+    const get = async (id: string) => {
+        const cacheRes = await redis.get(`server-${id}`);
+        if (cacheRes) {
+            return cacheRes;
+        } else {
+            const res = await fetch(
+                `https://discord.com/api/v10/guilds/${id}`,
+                {
+                    headers: {
+                        Authorization: `Bot ${c.env.DISCORD_TOKEN}`,
+                    },
+                },
+            );
+            if (!res.ok) {
+                return null;
+            }
+            const data = (await res.json()) as { icon: string };
+            const avatar = data.icon;
+            await redis.set(`server-${id}`, avatar, { ex: 60 });
+            return avatar;
+        }
+    };
+    const avatar = await get(id);
+    if (!avatar) {
+        return c.json({ error: "Server not found" }, { status: 404 });
+    }
+    return c.redirect(
+        `https://cdn.discordapp.com/icons/${id}/${avatar}.${format}?size=${resolution}`,
+        302,
+    );
 });
 
 export default app;
